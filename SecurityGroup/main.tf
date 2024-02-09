00b8ed4002671afd36888544bff4014a4d59db80
Tran <39490002+tboytboy@users.noreply.github.com>
2024-02-09 08:25:48 -0500

    Update main.tf

--- SecurityGroup/main.tf
+++ SecurityGroup/main.tf
@@ -1,1 +1,37 @@
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 4.0"
+   }
+ }
+}
 
+provider "aws" {
+ region = "us-east-1"
+}
+
+data "aws_vpc" "default" {
+ default = true
+}
+
+resource "aws_security_group" "web_server_sg_tf" {
+ name        = "web-server-sg-tf"
+ description = "Allow HTTPS to web server"
+ vpc_id      = data.aws_vpc.default.id
+
+ingress {
+   description = "HTTPS ingress"
+   from_port   = 443
+   to_port     = 443
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+
+egress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
